70d55433182562151e965fdf2ee80b66e65953f1
ntinakav <ntinakav@gmail.com>
2025-07-30 15:07:03 +0300

    webdev.html and more v.05

--- project/webdev.js
+++ project/webdev.js
@@ -17,4 +17,18 @@ function toggleButtonHandler() {
         main.style.display = "block";
         footer.style.display = "flex";
     }
+
+/* ΕΧΕΙΣ ΚΑΝΕΙ ΣΤΟ ΑΡΧΕΙΟ .css ΝΑ ΕΜΦΑΝΙΖΕΤΑΙ ΤΟ ΜΕΝΟΥ ΓΙΑ ΟΘΟΝΕΣ ΜΙΚΡΟΤΕΡΕΣ ΑΠΟ 992px. ΟΜΩΣ, ΑΝ ΕΙΣΑΙ ΣΕ ΜΙΚΡΗ ΟΘΟΝΗ ΚΑΙ ΠΑΤΗΣΕΙΣ ΤΟ toggle-button ΚΑΙ ΜΕΤΑ ΞΑΝΑΚΑΝΕΙΣ ΜΕΓΙΣΤΟΠΟΙΗΣΗ, ΕΠΑΝΕΡΧΕΤΑΙ ΤΟ ΑΡΧΙΚΟ ΜΕΝΟΥ ΓΙΑ ΜΕΓΑΛΗ ΟΘΟΝΗ, ΑΛΛΑ ΕΧΕΙ ΜΕΙΝΕΙ ΤΟ block ΑΠΟ ΤΟ TOGGLE MENU.
+ΓΙΑ ΝΑ ΤΟ ΔΙΟΡΘΩΣΕΙΣ ΑΥΤΟ, ΠΡΕΠΕΙ ΝΑ ΒΑΛΕΙΣ EVENT LISTENER resize, ΟΠΟΥ ΟΠΟΤΕ ΑΛΛΑΖΕΙΣ ΜΕΓΕΘΟΣ ΣΤΗ ΣΕΛΙΔΑ ΘΑ ΑΚΟΥΕΙ, ΚΑΙ ΑΝ Η ΣΕΛΙΔΑ ΕΙΝΑΙ ΠΑΝΩ ΑΠΟ 992px, ΘΑ ΕΠΑΝΑΦΕΡΕΙ ΤΟ toggle-menu σε display none.
+ΔΕΝ ΤΟ ΚΑΝΕΙ ΑΥΤΟΜΑΤΑ.
+ */
+
+    window.addEventListener("resize", () => {
+        if (window.innerWidth > 992) {
+            toggleMenu.style.display = "none"; 
+            slider.style.display = "block";
+            main.style.display = "block";
+            footer.style.display = "flex";
+        }
+    });
 }
